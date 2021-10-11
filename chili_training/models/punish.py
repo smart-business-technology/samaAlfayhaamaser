9fa13e760aa8a8cb3b9e694f30910ea86bc5b40c
fatmanedal <fatma-nedal@hotmail.com>
2021-10-12 00:00:23 +0200

    add changes

--- chili_training/models/punish.py
+++ chili_training/models/punish.py
@@ -16,9 +16,13 @@ class Award(models.Model):
     name = fields.Char(string="Name")
     employee_id = fields.Many2one("hr.employee", string="Employee")
     date = fields.Date(string="Date")
-    ttype = fields.Many2one('employee.award.type', string="نوع العقوبه")
+    ttype = fields.Many2one('employee.punish.type', string="نوع العقوبه")
     amount = fields.Float(string="Amount")
     description = fields.Text(string="Description")
     company_id = fields.Many2one('res.company',
                                  default=lambda self: self.employee_id.company_id)
     currency_id = fields.Many2one('res.currency', string='Currency', related='company_id.currency_id')
+class punish1(models.Model):
+    _name = 'employee.punish.type'
+
+    name = fields.Char()
--- emp_fields/models/emp_filds.py
+++ emp_fields/models/emp_filds.py
@@ -12,19 +12,20 @@ class employee_fields(models.Model):
     amount = fields.Char(string="amount", required=False, )
     notes = fields.Text(string="الملاحظات", required=False, )
     wosolat_ids = fields.One2many(comodel_name="wosolat.wosolat", inverse_name="emp_ids", string="دفتر الوصولات", required=False, )
-    # from1 = fields.Float(string="من",  required=False, )
-    # from2 = fields.Float(string="من",  required=False, )
-    # from3 = fields.Float(string="من",  required=False, )
-    # to1 = fields.Float(string="الي",  required=False, )
-    # to2 = fields.Float(string="الي",  required=False, )
-    # to3 = fields.Float(string="الي",  required=False, )
-    # date1 = fields.Date(string=" تاريخ الاستلام", required=False, )
-    # date2 = fields.Date(string=" تاريخ الاستلام", required=False, )
-    # date3 = fields.Date(string=" تاريخ الاستلام", required=False, )
+    solaf_ids = fields.One2many(comodel_name="solaf.solaf", inverse_name="emp_ids", string="السلف", required=False, )
 
 class wosolat(models.Model):
     _name = 'wosolat.wosolat'
     from1 = fields.Float(string="من",  required=False, )
     to1 = fields.Float(string="الي",  required=False, )
     date1 = fields.Date(string=" تاريخ الاستلام", required=False, )
+    emp_ids = fields.Many2one(comodel_name="hr.employee", string="", required=False, )
+
+class solaf(models.Model):
+    _name = 'solaf.solaf'
+    solaf = fields.Char(string="  مبلغ السلفة", required=False, )
+    kest = fields.Char(string="القسط الشهري", required=False, )
+
+    date1 = fields.Date(string=" تاريخ تسليم السلفة", required=False, )
+    date2 = fields.Date(string=" تاريخ إنتهاء السلفة", required=False, )
     emp_ids = fields.Many2one(comodel_name="hr.employee", string="", required=False, )
